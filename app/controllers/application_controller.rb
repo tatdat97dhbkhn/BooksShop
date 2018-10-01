d5ccf2cb1bd853c39ad7faca9e4faf1b0751e392
tatdat97dhbkhn <tatdat97dhbkhn@gmail.com>
2018-10-01 15:22:04 +0700

    display

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer
+      .permit(:sign_up){|u| u.permit User::USER_CREATE_PARAMS}
+    devise_parameter_sanitizer
+      .permit(:account_update){|u| u.permit User::USER_UPDATE_PARAMS}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  root "books#index"
+  resources :books
+  resources :authors
+  resources :categories
 end
