edcba527ac880d99da8dc8fcad4019cbf5eece5d
tatdat97dhbkhn <tatdat97dhbkhn@gmail.com>
2018-10-01 15:22:04 +0700

    sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,14 @@ class User < ApplicationRecord
 
   validates :name, presence: true,
     length: {maximum: Settings.user.max_name}
-
+  mount_uploader :image, PictureUploader
+  validate :image_size
   USER_CREATE_PARAMS = %w(name email password image).freeze
   USER_UPDATE_PARAMS = %w(password current_password).freeze
+
+  private
+  def image_size
+    return unless image.size > Settings.image.capacity.megabytes
+    errors.add :image, "should be less than 5MB"
+  end
 end
